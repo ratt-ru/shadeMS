b84128c38a0101ffe173f97148c3461483753a87
IanHeywood <ian.heywood@gmail.com>
2019-07-27 11:49:25 +0100

    Testing

--- shadems.py
+++ shadems.py
@@ -10,20 +10,54 @@ from datashader import transfer_functions as tf
 import holoviews as hv
 import holoviews.operation.datashader as hd
 import numpy
+import pylab
 import sys
-import xarrayms as xa
+import time
+import xarray as xa
+import xarrayms as xms
 
-hv.extension('matplotlib')
-#%%output backend='matplotlib'
-
+clock_start = time.time()
 
 myms = sys.argv[1]
 
-msdata = xa.xds_from_ms(myms,columns=['DATA','TIME'])
-msdata[0].DATA.values = numpy.abs(msdata[0].DATA.values)
+xaxis = 'TIME'
+yaxis = 'DATA'
+corr = 0
+spw = 0
+doplot = 'amp'
 
+
+msdata = xms.xds_from_ms(myms,columns=[yaxis,'TIME','FLAG'])
+
+for data in msdata:
+	data.rename({yaxis:'VISDATA'},inplace=True)
+
+if doplot == 'amp':
+	visdata = numpy.abs(msdata[spw].VISDATA.values[:,:,corr])
+
+nchan = visdata.shape[1]
+visdata = numpy.ravel(visdata)
+xdata = numpy.repeat(msdata[spw].TIME.values,nchan)
 
 canvas = ds.Canvas(1024,512)
-agg = canvas.points(msdata[0],'TIME','DATA')
-export_image(tf.set_background(tf.shade(agg, cmap=colorcet.bmy,how='log'),"black"),'test')
-#export_image(tf.shade(agg),'test.png')
+agg = canvas.points(msdata[spw],xaxis,yaxis)
+img = hd.shade(hv.Image(agg),cmap=colorcet.bmy)
+#img = tf.set_background(tf.shade(agg, cmap=colorcet.dimgray,how='log'),"black")
+	
+
+fig = pylab.figure(figsize=(24,12))
+ax = fig.add_subplot(111)
+ax.set_facecolor('black')
+
+ymin = numpy.min(agg.coords[yaxis].values)
+ymax = numpy.max(agg.coords[yaxis].values)
+xmin = numpy.min(agg.coords[xaxis].values)
+xmax = numpy.max(agg.coords[xaxis].values)
+
+ax.imshow(X=img.data,extent=[xmin,xmax,ymin,ymax],aspect='auto',origin='upper',cmap='gist_heat')
+
+fig.savefig('test.png',bbox_inches='tight')
+
+clock_stop = time.time()
+elapsed = str(round((clock_stop-clock_start),2))
+print 'Plot was rendered in '+elapsed+' seconds.'
