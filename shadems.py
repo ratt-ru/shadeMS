9a9db777ac3b44bd2cf9b9536c61ac6377eae69e
IanHeywood <ian.heywood@gmail.com>
2019-07-27 12:40:17 +0100

    First rough working version

--- shadems.py
+++ shadems.py
@@ -2,6 +2,7 @@
 # pip install datashader
 # pip install holoviews
 
+from collections import OrderedDict as odict
 import colorcet
 import dask.dataframe as dd
 import datashader as ds
@@ -10,6 +11,8 @@ from datashader import transfer_functions as tf
 import holoviews as hv
 import holoviews.operation.datashader as hd
 import numpy
+import os
+import pandas as pd
 import pylab
 import sys
 import time
@@ -18,36 +21,51 @@ import xarrayms as xms
 
 clock_start = time.time()
 
-myms = sys.argv[1]
+myms = sys.argv[1].rstrip(os.sep)
 
 xaxis = 'TIME'
 yaxis = 'DATA'
-corr = 0
+corr = 2
 spw = 0
 doplot = 'amp'
+normalize = 'eq_hist'
 
 
 msdata = xms.xds_from_ms(myms,columns=[yaxis,'TIME','FLAG'])
 
+
 for data in msdata:
 	data.rename({yaxis:'VISDATA'},inplace=True)
 
+
 if doplot == 'amp':
 	visdata = numpy.abs(msdata[spw].VISDATA.values[:,:,corr])
+elif doplot == 'phase':
+	visdata = numpy.angle(msdata[spw].VISDATA.values[:,:,corr])
+elif doplot == 'real':
+	visdata = numpy.real(msdata[spw].VISDATA.values[:,:,corr])
+elif doplot == 'imag':
+	visdata = numpy.imag(msdata[spw].VISDATA.values[:,:,corr])
+
 
 nchan = visdata.shape[1]
 visdata = numpy.ravel(visdata)
 xdata = numpy.repeat(msdata[spw].TIME.values,nchan)
 
-canvas = ds.Canvas(1024,512)
-agg = canvas.points(msdata[spw],xaxis,yaxis)
-img = hd.shade(hv.Image(agg),cmap=colorcet.bmy)
+
+dists = {'plotdata': pd.DataFrame(odict([(xaxis,xdata),(yaxis,visdata)]))}
+df = pd.concat(dists,ignore_index=True)
+
+
+canvas = ds.Canvas(2048,1024)
+agg = canvas.points(df,xaxis,yaxis)
+img = hd.shade(hv.Image(agg),cmap=colorcet.bkr,normalization=normalize)
 #img = tf.set_background(tf.shade(agg, cmap=colorcet.dimgray,how='log'),"black")
 	
 
-fig = pylab.figure(figsize=(24,12))
+fig = pylab.figure(figsize=(18,8))
 ax = fig.add_subplot(111)
-ax.set_facecolor('black')
+#ax.set_facecolor('black')
 
 ymin = numpy.min(agg.coords[yaxis].values)
 ymax = numpy.max(agg.coords[yaxis].values)
@@ -55,6 +73,10 @@ xmin = numpy.min(agg.coords[xaxis].values)
 xmax = numpy.max(agg.coords[xaxis].values)
 
 ax.imshow(X=img.data,extent=[xmin,xmax,ymin,ymax],aspect='auto',origin='upper',cmap='gist_heat')
+
+ax.set_title(myms)
+ax.set_xlabel(xaxis.capitalize())
+ax.set_ylabel(yaxis+' '+doplot.capitalize())
 
 fig.savefig('test.png',bbox_inches='tight')
 
