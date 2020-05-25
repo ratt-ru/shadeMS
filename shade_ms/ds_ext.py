95080a97d5e32eec37811ed63b24783c59c47879
Oleg Smirnov <osmirnov@gmail.com>
2020-05-25 22:30:19 +0200

    updated reductions for compaitiblity with ds-0.11.x

--- shade_ms/ds_ext.py
+++ shade_ms/ds_ext.py
@@ -1,106 +1,106 @@
 # -*- coding: future_fstrings -*-
 
-import datashape.coretypes
-import datashader.transfer_functions
-import datashader.reductions
-
-import xarray
-import datashape.coretypes
-import datashader.transfer_functions
+import datashape
+import datashape.coretypes as ct
 import datashader.reductions
 import numpy as np
-from datashader.utils import ngjit
-from datashader.reductions import Preprocess
 
 try:
     import cudf
 except ImportError:
     cudf = None
 
-# class count_integers(datashader.count_cat):
-#     """Aggregator. Counts of all elements in ``column``, grouped by value of another column. Like datashader.count_cat,
-#     but for normal columns."""
-#     _dshape = datashape.dshape(datashape.coretypes.int32)
-#
-#     def __init__(self, column, modulo):
-#         datashader.count_cat.__init__(self, column)
-#         self.modulo = modulo
-#         self.codes = xarray.DataArray(list(range(self.modulo)))
-#
-#     def validate(self, in_dshape):
-#         pass
-#
-#     @property
-#     def inputs(self):
-#         return (datashader.reductions.extract(self.column), )
-#
-#     @staticmethod
-#     @ngjit
-#     def _append(x, y, agg, field):
-#         agg[y, x, int(field)] += 1
-#
-#     def out_dshape(self, input_dshape):
-#         return datashape.util.dshape(datashape.Record([(c, datashape.coretypes.int32) for c in range(self.modulo)]))
-#
-#     def _build_finalize(self, dshape):
-#         def finalize(bases, cuda=False, **kwargs):
-#             dims = kwargs['dims'] + [self.column]
-#             coords = kwargs['coords']
-#             coords[self.column] = list(self.codes.values)
-#             return xarray.DataArray(bases[0], dims=dims, coords=coords)
-#         return finalize
+# couldn't find anything in the datashape docs about how to check if a CType is an integer,
+# so just define a big set
+IntegerTypes = {ct.bool_, ct.uint8, ct.uint16, ct.uint32, ct.uint64, ct.int8, ct.int16, ct.int32, ct.int64}
 
 
-class extract_multi(datashader.reductions.category_values):
-    """Extract multiple columns from a dataframe as a numpy array of values. Like datashader.category_values,
-    but with two columns."""
-    def apply(self, df):
-        if cudf and isinstance(df, cudf.DataFrame):
-            import cupy
-            cols = []
-            for column in self.columns:
-                nullval = np.nan if df[self.columns[1]].dtype.kind == 'f' else 0
-                cols.append(df[self.columns[0]].to_gpu_array(fillna=nullval))
-            return cupy.stack(cols, axis=-1)
-        else:
-            return np.stack([df[col].values for col in self.columns], axis=-1)
+def _column_modulo(df, column, modulo):
+    """
+    Helper function. Takes a DataFrame column, modulo integer value
+    """
+    if cudf and isinstance(df, cudf.DataFrame):
+        ## dunno how to do this in CUDA
+        raise NotImplementedError("this feature is not implemented in cudf")
+        ## but is it as simple as this?
+        # return (df[column] % modulo).to_gpu_array()
+    else:
+        return df[column].values % modulo
 
-class integer_modulo(Preprocess):
+
+def _column_discretization(df, column, offset, delta, nsteps):
+    """
+    Helper function. Takes a DataFrame column, modulo integer value
+    """
+    if cudf and isinstance(df, cudf.DataFrame):
+        ## dunno how to do this in CUDA
+        raise NotImplementedError("this feature is not implemented in cudf")
+    else:
+        value = df[column].values
+        index = ((value - offset) / delta).astype(np.uint32)
+        index[index < 0] = 0
+        index[index >= nsteps] = nsteps - 1
+        index[np.isnan(value)] = nsteps
+        return index
+
+
+class integer_modulo(datashader.reductions.category_codes):
+    """A variation on category_codes that replaces categories by the values from an integer column, modulo a certain number"""
     def __init__(self, column, modulo):
         super().__init__(column)
         self.modulo = modulo
 
-    """Extract just the category codes from a categorical column."""
     def apply(self, df):
-        if cudf and isinstance(df, cudf.DataFrame):
-            return (df[self.column] % self.modulo).to_gpu_array()
-        else:
-            return df[self.column].values % self.modulo
+        return _column_discretization(df, self.column, self.modulo)
 
-class integer_modulo_values(Preprocess):
-    """Extract multiple columns from a dataframe as a numpy array of values."""
+
+class float_discretization(integer_modulo):
+    """A variation on category_codes that replaces categories by the values from an integer column, modulo a certain number"""
+    def __init__(self, column, offset, delta, nsteps):
+        super().__init__(column, nsteps)
+        self.offset = offset
+        self.delta = delta
+        self.nsteps = nsteps
+
+    def apply(self, df):
+        return _column_discretization(df, self.column, self.offset, self.delta, self.nsteps)
+
+
+class integer_modulo_values(datashader.reductions.category_values):
+    """A variation on category_values that replaces categories by the values from an integer column, modulo a certain number"""
     def __init__(self, columns, modulo):
-        self.columns = list(columns)
+        super().__init__(columns)
         self.modulo = modulo
 
-    @property
-    def inputs(self):
-        return self.columns
-
     def apply(self, df):
+        a = _column_modulo(df, self.columns[0], self.modulo)
+        return self._attach_value(df, a)
+
+    def _attach_value(self, df, a):
         if cudf and isinstance(df, cudf.DataFrame):
             import cupy
             if df[self.columns[1]].dtype.kind == 'f':
                 nullval = np.nan
             else:
                 nullval = 0
-            a = (df[self.columns[0]] % self.modulo).to_gpu_array()
             b = df[self.columns[1]].to_gpu_array(fillna=nullval)
             return cupy.stack((a, b), axis=-1)
         else:
-            a = df[self.columns[0]].values % self.modulo
             b = df[self.columns[1]].values
             return np.stack((a, b), axis=-1)
+
+
+class float_discretization_values(integer_modulo_values):
+    """A variation on category_codes that replaces categories by the values from an integer column, modulo a certain number"""
+    def __init__(self, columns, offset, delta, nsteps):
+        super().__init__(columns, nsteps+1)  # one extra category for NaNs
+        self.offset = offset
+        self.delta = delta
+        self.nsteps = nsteps
+
+    def apply(self, df):
+        a = _column_discretization(df, self.columns[0], self.offset, self.delta, self.nsteps)
+        return self._attach_value(df, a)
 
 
 class by_integers(datashader.by):
@@ -115,7 +115,8 @@ class by_integers(datashader.by):
     def validate(self, in_dshape):
         if not self.cat_column in in_dshape.dict:
             raise ValueError("specified column not found")
-
+        if not in_dshape.measure[self.cat_column] in IntegerTypes:
+            raise ValueError("input must be an integer column")
         self.reduction.validate(in_dshape)
 
     def out_dshape(self, input_dshape):
@@ -147,7 +148,7 @@ class by_integers(datashader.by):
         return finalize
 
 class by_span(by_integers):
-    """Like datashader.by, but for float-valued columns."""
+    """Like datashader.by, but for float-valued columns, discretized by steps over a certain span"""
     def __init__(self, cat_column, reduction, offset, delta, nsteps):
         super().__init__(cat_column, reduction, nsteps+1)  # allocate extra category for NaNs
         self.offset = offset
@@ -155,7 +156,20 @@ class by_span(by_integers):
         self.nsteps = nsteps
 
     def _build_temps(self, cuda=False):
-        return tuple(by_span(self.cat_column, tmp, self.offset, self.delta, self.nsteps) for tmp in self.reduction._build_temps(cuda))
+        return tuple(by_span(self.cat_column, tmp, self.offset, self.delta, self.nsteps)
+                     for tmp in self.reduction._build_temps(cuda))
+
+    def validate(self, in_dshape):
+        if not self.cat_column in in_dshape.dict:
+            raise ValueError("specified column not found")
+        self.reduction.validate(in_dshape)
+
+    @property
+    def inputs(self):
+        if self.val_column is not None:
+            return (float_discretization_values(self.columns, self.offset, self.delta, self.nsteps),)
+        else:
+            return (float_discretization(self.columns[0], self.offset, self.delta, self.nsteps),)
 
     def _build_bases(self, cuda=False):
         bases = self.reduction._build_bases(cuda)
@@ -163,30 +177,3 @@ class by_span(by_integers):
             return bases
         return tuple(by_span(self.cat_column, base, self.offset, self.delta, self.nsteps) for base in bases)
 
-    def _build_append(self, dshape, schema, cuda=False):
-        f = self.reduction._build_append(dshape, schema, cuda)
-        # because we transposed, we also need to flip the
-        # order of the x/y arguments
-        if isinstance(self.reduction, datashader.reductions.m2):
-            def _categorical_append(x, y, agg, cols, tmp1, tmp2, minval=self.offset, d=self.delta, n=self.nsteps):
-                _agg = agg.transpose()
-                value = cols[0]
-                _ind = min(max(0, int((value - minval)/d)), n-1) if value == value else n  # value != itself when NaN
-                #print("a", _ind, cols[0])
-                f(y, x, _agg[_ind], cols[1], tmp1[_ind], tmp2[_ind])
-        elif self.val_column is not None:
-            def _categorical_append(x, y, agg, field, minval=self.offset, d=self.delta, n=self.nsteps):
-                _agg = agg.transpose()
-                value = field[0]
-                _ind = min(max(0, int((value - minval)/d)), n-1) if value == value else n  # value != itself when NaN
-                #print("b", _ind, field[0])
-                f(y, x, _agg[_ind], field[1])
-        else:
-            def _categorical_append(x, y, agg, field, minval=self.offset, d=self.delta, n=self.nsteps):
-                _agg = agg.transpose()
-                value = field
-                _ind = min(max(0, int((value - minval)/d)), n-1) if value == value else n  # value != itself when NaN
-                #print("c", _ind, field, field != field)
-                f(y, x, _agg[_ind])
-
-        return ngjit(_categorical_append)
