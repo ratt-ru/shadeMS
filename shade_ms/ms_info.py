4bb61326524de5e2e207328c6396a00cff438ae5
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2023-03-15 14:09:16 +0200

    Remove msutils from dep list: Provides additionall requiremnts and conflicts

--- shade_ms/ms_info.py
+++ shade_ms/ms_info.py
@@ -1,10 +1,45 @@
-from MSUtils.msutils import STOKES_TYPES
 from casacore.tables import table
 import re
 import daskms
 import math
 import numpy as np
 from collections import OrderedDict
+
+STOKES_TYPES = {
+    0  : "Undefined",
+    1  : "I",
+    2  : "Q",
+    3  : "U",
+    4  : "V",
+    5  : "RR",
+    6  : "RL",
+    7  : "LR",
+    8  : "LL",
+    9  : "XX",
+    10 : "XY",
+    11 : "YX",
+    12 : "YY",
+    13 : "RX",
+    14 : "RY",
+    15 : "LX",
+    16 : "LY",
+    17 : "XR",
+    18 : "XL",
+    19 : "YR",
+    20 : "YL",
+    21 : "PP",
+    22 : "PQ",
+    23 : "QP",
+    24 : "QQ",
+    25 : "RCircular",
+    26 : "LCircular",
+    27 : "Linear",
+    28 : "Ptotal",
+    29 : "Plinear",
+    30 : "PFtotal",
+    31 : "PFlinear",
+    32 : "Pangle",
+}
 
 class NamedList(object):
     """Holds a list of names (e.g. field names), and provides common indexing and subset operations"""
